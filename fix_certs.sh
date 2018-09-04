90a66cf24649150b90422c658c9f4e8a39bebe9f
efajardo <efajardo@physics.ucsd.edu>
2018-09-04 10:37:02 -0700

    Typo to create the xrd certs

--- fix_certs.sh
+++ fix_certs.sh
@@ -2,7 +2,7 @@
 CERT_DIR=/etc/grid-security/
 mkdir -p $CERT_DIR/xrd
 cp -f $CERT_DIR/hostcert.pem /etc/grid-security/xrd/xrdcert.pem
-cp -f $CERT_DIR/xrdkey.pem /etc/grid-security/xrd/xrdkey.pem
+cp -f $CERT_DIR/hostkey.pem /etc/grid-security/xrd/xrdkey.pem
 chmod 644  /etc/grid-security/xrd/xrdcert.pem
 chmod 600 /etc/grid-security/xrd/xrdkey.pem
 chown xrootd:xrootd /etc/grid-security/xrd/xrd*.pem
