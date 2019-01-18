25102056e833ec17d73efed3887b67e5370b8005
Brian Lin <brianhlin@gmail.com>
2019-01-18 14:45:35 -0600

    Only xrd{cert,key} are required; dirs are created by the xcache RPM

--- xcache/sbin/fix_certs.sh
+++ xcache/sbin/fix_certs.sh
@@ -1,8 +1,4 @@
 #!/bin/bash                                                                                                                                                                   
-CERT_DIR=/etc/grid-security/
-mkdir -p $CERT_DIR/xrd
-cp -f $CERT_DIR/hostcert.pem /etc/grid-security/xrd/xrdcert.pem
-cp -f $CERT_DIR/hostkey.pem /etc/grid-security/xrd/xrdkey.pem
 chmod 644  /etc/grid-security/xrd/xrdcert.pem
 chmod 600 /etc/grid-security/xrd/xrdkey.pem
 chown xrootd:xrootd /etc/grid-security/xrd/xrd*.pem
