57654320899fbfaffb2d75a20179c67994bb1896
Brian Lin <brianhlin@gmail.com>
2019-01-30 10:02:25 -0600

    Copy certs after container start; allow users to mount certs

--- xcache/sbin/fix_certs.sh
+++ xcache/sbin/fix_certs.sh
@@ -1,4 +1,7 @@
-#!/bin/bash                                                                                                                                                                   
+#!/bin/bash -xe
+
+cp /etc/grid-security/{hostcert.pem,xrd/xrdcert.pem}
+cp /etc/grid-security/{hostkey.pem,xrd/xrdkey.pem}
 chmod 644  /etc/grid-security/xrd/xrdcert.pem
 chmod 600 /etc/grid-security/xrd/xrdkey.pem
 chown xrootd:xrootd /etc/grid-security/xrd/xrd*.pem
