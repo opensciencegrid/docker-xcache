68de2433d60fa51fd3a36ffe4c4a021d4c1c941c
Brian Lin <brianhlin@gmail.com>
2019-01-31 15:43:21 -0600

    Combine docker build/push into a single job; Add timestamp tags
    This sets us up for inserting tests after the build step that will
    determine whether or not we push to DockerHub

--- travis/build_docker.sh
+++ travis/build_docker.sh
@@ -1,8 +1,21 @@
 #!/bin/bash -xe
 # Script for building and pushing XCache docker images
 
-IMAGE_NAME="$1"
+org='opensciencegrid'
+timestamp=`date +%Y%m%d-%H%M`
 
-docker build -t opensciencegrid/$IMAGE_NAME:development $IMAGE_NAME
+for repo in xcache stash-cache atlas-xcache; do
+    docker build \
+           -t $org/$repo:development \
+           -t $org/$repo:$timestamp \
+           $repo
+done
+
+# Credentials for docker push
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push opensciencegrid/$IMAGE_NAME:development
+
+for repo in xcache stash-cache atlas-xcache; do
+    for tag in development $timestamp; do
+        docker push $org/$repo:$tag
+    done
+done
