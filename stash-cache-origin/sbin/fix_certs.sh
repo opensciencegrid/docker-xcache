1d86768a0f5f12d11242a98f2a85a8deb042529b
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-02-21 14:26:08 -0800

    Move common functionality in xcache

--- stash-cache-origin/sbin/fix_certs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash                                                                                                                                                                   
-chmod 644  /etc/grid-security/xrd/xrdcert.pem
-chmod 600 /etc/grid-security/xrd/xrdkey.pem
-chown xrootd:xrootd /etc/grid-security/xrd/xrd*.pem
--- xcache/sbin/supervisord_startup.sh
+++ xcache/sbin/supervisord_startup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-/usr/sbin/setup_xrootd_certs.sh
+# Allow the derived images to run any additional runtime costumizations
+source /usr/local/sbin/xcache_image_init.sh
 
 # Give the chance to the pod to initialize host-specific info
-source /usr/sbin/pod_init.sh
+source /usr/local/sbin/xcache_pod_init.sh
 
 # Now we can actually start the supervisor
 exec /usr/bin/supervisord -c /etc/supervisord.conf
