24f3d2f57eca7bef81c345790e2724753ac630bb
efajardo <efajardo@physics.ucsd.edu>
2019-11-07 15:30:35 -0800

    Checking everything is up and runnng

--- travis/test_stashcache.sh
+++ travis/test_stashcache.sh
@@ -10,6 +10,6 @@ docker run --rm --publish 1094:1094 \
        --volume $(origin-config)/authfile:/etc/xrootd/public-origin-authfile \
        --name test_origin opensciencegrid/stash-origin:fresh &
 docker ps 
-docker exec -it test_origin ps aux | grep xrootd
+docker exec -it test_origin ps aux
 
 
