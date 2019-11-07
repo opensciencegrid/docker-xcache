8ed00346debf6f2b41f36e2bfa30ed01119d2062
efajardo <efajardo@physics.ucsd.edu>
2019-11-07 15:20:32 -0800

    Adding the configuration for the origin container

--- travis/test_stashcache.sh
+++ travis/test_stashcache.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -xe
 # Script for testing StashCache docker images
+
+docker run --rm --publish 1094:1094 \
+       --env-file=$(pwd)/stashche-origin-config/origin-env \
+       --volume $(pwd)/stashche-origin-config/empty_stash-origin-auth.conf:/etc/supervisord.d/stash-origin-auth.conf \
+       --volume $(pwd)/stashche-origin-config/10-origin-authfile.cfg:/etc/xrootd/config.d/10-origin-authfile.cfg \
+       --volume  $(pwd)/stashche-origin-config/authfile:/etc/xrootd/public-origin-authfile \
+       --name test_origin opensciencegrid/stash-origin:fresh &
+docker ps 
+docker exec -it test_origin ps aux | grep xrootd
+
+
