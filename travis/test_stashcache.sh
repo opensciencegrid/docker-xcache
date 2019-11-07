1eb94645cfbcc114beb6a059d1f1f27cf854996f
efajardo <efajardo@physics.ucsd.edu>
2019-11-07 15:40:00 -0800

    fixing the paths of the config

--- travis/test_stashcache.sh
+++ travis/test_stashcache.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -xe
 # Script for testing StashCache docker images
 
-origin-config=$(pwd)/stashcache-origin-config
 
 docker run --rm --publish 1094:1094 \
-       --env-file=$(origin-config)/origin-env \
-       --volume $(origin-config)/empty_stash-origin-auth.conf:/etc/supervisord.d/stash-origin-auth.conf \
-       --volume $(origin-config)/10-origin-authfile.cfg:/etc/xrootd/config.d/10-origin-authfile.cfg \
-       --volume $(origin-config)/authfile:/etc/xrootd/public-origin-authfile \
+       --env-file=$(pwd)/travis/stashcache-origin-config/origin-env \
+       --volume $(pwd)/travis/stashcache-origin-config/empty_stash-origin-auth.conf:/etc/supervisord.d/stash-origin-auth.conf \
+       --volume $(pwd)/travis/stashcache-origin-config/10-origin-authfile.cfg:/etc/xrootd/config.d/10-origin-authfile.cfg \
+       --volume $(pwd)/travis/stashcache-origin-config/authfile:/etc/xrootd/public-origin-authfile \
        --name test_origin opensciencegrid/stash-origin:fresh &
 docker ps 
 docker exec -it test_origin ps aux
