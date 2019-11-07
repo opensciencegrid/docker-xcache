fbe6f1e71a259c258d1d52d7be412d692901a4bc
efajardo <efajardo@physics.ucsd.edu>
2019-11-07 15:27:33 -0800

    typo in origins

--- travis/test_stashcache.sh
+++ travis/test_stashcache.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -xe
 # Script for testing StashCache docker images
 
+origin-config=$(pwd)/stashcache-origin-config
+
 docker run --rm --publish 1094:1094 \
-       --env-file=$(pwd)/stashche-origin-config/origin-env \
-       --volume $(pwd)/stashche-origin-config/empty_stash-origin-auth.conf:/etc/supervisord.d/stash-origin-auth.conf \
-       --volume $(pwd)/stashche-origin-config/10-origin-authfile.cfg:/etc/xrootd/config.d/10-origin-authfile.cfg \
-       --volume  $(pwd)/stashche-origin-config/authfile:/etc/xrootd/public-origin-authfile \
+       --env-file=$(origin-config)/origin-env \
+       --volume $(origin-config)/empty_stash-origin-auth.conf:/etc/supervisord.d/stash-origin-auth.conf \
+       --volume $(origin-config)/10-origin-authfile.cfg:/etc/xrootd/config.d/10-origin-authfile.cfg \
+       --volume $(origin-config)/authfile:/etc/xrootd/public-origin-authfile \
        --name test_origin opensciencegrid/stash-origin:fresh &
 docker ps 
 docker exec -it test_origin ps aux | grep xrootd
