7aebe4f927c5292e0a5eb3b0c7a880369a6366f5
efajardo <efajardo@physics.ucsd.edu>
2019-11-18 14:52:40 -0800

    Testing removing the supervisord file for stashcache

--- travis/test_stashcache.sh
+++ travis/test_stashcache.sh
@@ -5,7 +5,6 @@
 docker run --rm \
        --network="host" \
        --env-file=$(pwd)/travis/stashcache-cache-config/cache-env \
-       --volume $(pwd)/travis/stashcache-cache-config/10-stash-cache.conf:/etc/supervisord.d/10-stash-cache.conf \
        --volume $(pwd)/travis/stashcache-cache-config/90-docker-ci.cfg:/etc/xrootd/config.d//90-docker-ci.cfg  \
        --name test_cache opensciencegrid/stash-cache:fresh &
 docker ps 
