c86bc497f731ec4c4aca940e4fae51346dea91bf
efajardo <efajardo@physics.ucsd.edu>
2020-03-04 12:53:05 -0800

    Giving a default AUthfile for travis ci tests

--- travis/test_stashcache.sh
+++ travis/test_stashcache.sh
@@ -6,6 +6,7 @@ docker run --rm \
        --network="host" \
        --env-file=$(pwd)/travis/stashcache-cache-config/cache-env \
        --volume $(pwd)/travis/stashcache-cache-config/90-docker-ci.cfg:/etc/xrootd/config.d//90-docker-ci.cfg  \
+       --volume $(pwd)/travis/stashcache-cache-config/Authfile:/run/stash-cache/Authfile \
        --name test_cache opensciencegrid/stash-cache:fresh &
 docker ps 
 sleep 20
