0a054472edf7ac2266d85c3bec94746ad35e5830
efajardo <efajardo@physics.ucsd.edu>
2020-03-04 17:38:53 -0800

    Forgot to change the port 8000 on the test

--- travis/test_stashcache.sh
+++ travis/test_stashcache.sh
@@ -12,7 +12,7 @@ docker ps
 sleep 25
 curl -v -sL http://localhost:8000/stashcache-travis-ci-test/test_file
 
-online_md5="$(curl -sL http://localhost:10001/stashcache-travis-ci-test/test_file | md5sum | cut -d ' ' -f 1)"
+online_md5="$(curl -sL http://localhost:8000/stashcache-travis-ci-test/test_file | md5sum | cut -d ' ' -f 1)"
 local_md5="$(md5sum $(pwd)/travis/stashcache-origin-config/test_file | cut -d ' ' -f 1)"
 if [ "$online_md5" != "$local_md5" ]; then
     echo "MD5sums do not match on stashcache"
