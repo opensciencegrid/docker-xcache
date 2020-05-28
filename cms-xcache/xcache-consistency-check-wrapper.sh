402b7f96d422b01f384334a48fe64bf0ff9f2cc1
Diego Davila <davila.foyo@gmail.com>
2020-05-28 12:17:53 -0500

    better starting message in wrapper

--- cms-xcache/xcache-consistency-check-wrapper.sh
+++ cms-xcache/xcache-consistency-check-wrapper.sh
@@ -3,7 +3,7 @@ ENABLE=`awk -F "=" '/^XC_XCACHE_CONSISTENCY_CHECK/ {print $2}' /etc/environment`
 
 if [ "$ENABLE" == "1" ]
 then
-    echo "Starting"
+    echo "Starting XCache consistency check..."
     export PYTHONPATH=/usr/lib/xcache-consistency-check/usr/lib/python2.7/site-packages/:/usr/lib/xcache-consistency-check/usr/lib64/python2.7/site-packages/
     /usr/bin/xcache-consistency-check --config /etc/xrootd/xcache-consistency-check.cfg
 else
