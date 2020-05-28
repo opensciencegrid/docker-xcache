147c719b8f6086ec24584c802aaeccd500405570
ddavila0 <davila.foyo@gmail.com>
2020-05-28 09:57:16 -0700

    Update cms-xcache/xcache-consistency-check-wrapper.sh
    Co-authored-by: Brian Lin <brianhlin@gmail.com>

--- cms-xcache/xcache-consistency-check-wrapper.sh
+++ cms-xcache/xcache-consistency-check-wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ENABLE=`cat /etc/environment |  grep XC_XCACHE_CONSISTENCY_CHECK | awk -F "=" '{print $2}'`
+ENABLE=`awk -F "=" '/^XC_XCACHE_CONSISTENCY_CHECK/ {print $2}' /etc/environment`
 echo "ENABLE: "$ENABLE
 
 if [ "$ENABLE" == "1" ]
@@ -8,4 +8,3 @@ then
     export PYTHONPATH=/usr/lib/xcache-consistency-check/usr/lib/python2.7/site-packages/:/usr/lib/xcache-consistency-check/usr/lib64/python2.7/site-packages/
     /usr/bin/xcache-consistency-check --config /etc/xrootd/xcache-consistency-check.cfg
 fi
-
