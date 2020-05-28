dc0f2775636c7f1a86f65036e03aba701f0bdf55
ddavila0 <davila.foyo@gmail.com>
2020-05-28 09:58:05 -0700

    Update cms-xcache/xcache-consistency-check-wrapper.sh
    Co-authored-by: Brian Lin <brianhlin@gmail.com>

--- cms-xcache/xcache-consistency-check-wrapper.sh
+++ cms-xcache/xcache-consistency-check-wrapper.sh
@@ -6,4 +6,6 @@ then
     echo "Starting"
     export PYTHONPATH=/usr/lib/xcache-consistency-check/usr/lib/python2.7/site-packages/:/usr/lib/xcache-consistency-check/usr/lib64/python2.7/site-packages/
     /usr/bin/xcache-consistency-check --config /etc/xrootd/xcache-consistency-check.cfg
+else
+    echo "XCache consistency check disabled via the environment (XC_XCACHE_CONSISTENCY_CHECK=$ENABLE)..."
 fi
