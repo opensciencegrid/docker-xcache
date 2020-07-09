e79c5e474ad3c101b4326fdc549c993c481ded77
Brian Lin <brianhlin@gmail.com>
2020-07-09 15:20:46 -0500

    Fix dir creation for single and multi host disk use cases
    (SOFTWARE-4159)
    
    For the multi-disk case, we can be prescriptive and tell users where
    to mount their data, meta, and namespace (i.e. oss.localroot ) disks,
    so we can create the necessary directories in the image
    itself. N.B. oss.space directives will create the meta/data dirs
    in the paths specified in the second argument so we only have to worry
    about creating the namespace dir. This works nicely for the ATLAS
    XCache case since they use multiple disks and they're overriding our
    supervisor usage so we need this config baked into the image itself.
    
    In the single-disk case, users should be allowed to mount their disk
    to /xcache and get everything they need (meta, data, namespace)
    without having to worry about having an empty disk to begin
    with. In this case, the host dir's contents would override anything
    that we create in /xcache so we need to create the namespace dir after
    container startup, so we do this in a supervisor startup script.

--- xcache/image-config.d/10-disks-xcache.sh
+++ xcache/image-config.d/10-disks-xcache.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+# If the user is mounting a single disk onto /xcache, create oss.localroot for them
+# (image default: /xcache/namespace).
+# We don't need to do this for oss.space directives since XRootD will automatically
+# create the meta and data dirs automatically
 namespace_dir="$XC_ROOTDIR"
-disk_dir=$(dirname $namespace_dir)/disks
+mkdir -p $namespace_dir
+chown xrootd:xrootd $namespace_dir
 
-host_mounted_disks=$(ls $disk_dir)
-
-if [[ -z $host_mounted_disks ]]; then
-    # user has not mounted a separate data disks
-    echo "oss.space data $namespace_dir" > /etc/xrootd/config.d/40-data-disks.cfg
-else
-    for disk in $host_mounted_disks; do
-        echo "oss.space data $disk_dir/$disk" >> /etc/xrootd/config.d/40-data-disks.cfg
-    done
+# Use /xcache as the data store if the user has not mounted separate data disks
+# as prescribed (e.g. /xcache/disk1, /xcache/disk2/.../xcache/diskN)
+cache_dir=$(dirname $namespace_dir)
+if [[ -z $(find $cache_dir -type d  -name 'disk[0-9]*') ]]; then
+    echo "oss.space data $cache_dir" > /etc/xrootd/config.d/40-data-disks.cfg
 fi
-
 
 if [ -n "$DISABLE_OSG_MONITORING" ]; then
     echo -e "set DisableOsgMonitoring = $DISABLE_OSG_MONITORING\n" >> /etc/xrootd/config.d/10-docker-env-var.cfg
