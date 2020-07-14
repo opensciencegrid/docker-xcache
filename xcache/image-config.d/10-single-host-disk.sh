c7a4a37f725fe456409944139834f41aa2ff1bc3
Brian Lin <brianhlin@gmail.com>
2020-07-14 13:49:11 -0500

    Detect oss.space dirs by reading the XRootD config
    And only create the default data/meta dirs if none of the directories
    specified by oss.space directives exist
    
    1. /etc/xrootd/xrootd-$XC_IMAGE_NAME.cfg is used to determine the
    appropriate XRootD config
    2. This commit adds support for multiple metadata directories
    3. As a result, we change the accepted data directories from
    /xcache/disk* to /xcache/data* for clarity

--- xcache/image-config.d/10-single-host-disk.sh
+++ xcache/image-config.d/10-single-host-disk.sh
@@ -2,17 +2,31 @@
 
 # If the user is mounting a single disk onto /xcache, create oss.localroot for them
 # (image default: /xcache/namespace).
-# We don't need to do this for oss.space directives since XRootD will automatically
-# create the meta and data dirs automatically
 namespace_dir="$XC_ROOTDIR"
 mkdir -p $namespace_dir
 chown xrootd:xrootd $namespace_dir
 
-# Ensure that a data disk dir exists using the prescribed format
+# Ensure that data and meta disk dirs exist using the prescribed format
 # This allows users to easily transition to a multi-disk setup
 cache_dir=$(dirname $namespace_dir)
-if [[ -z $(find $cache_dir -type d  -name 'disk[0-9]*') ]]; then
-    data_dir=$cache_dir/disk1
-    mkdir -p $data_dir
-    chown -R xrootd:xrootd $data_dir
-fi
+
+for dirtype in meta data; do
+    # Requires $XC_ROOT_DIR to be set in container environment, which
+    # is set by default in the atlas-xcache, cms-xcache, stash-cache,
+    # and stash-origin images. oss.space directives can look like:
+    # oss.space data /xcache/data*
+    # oss.space meta /xcache/disk*
+    space_dirs=$(cconfig -c "/etc/xrootd/xrootd-$XC_IMAGE_NAME.cfg" 2>&1 \
+                     | awk "/^oss.space $dirtype/ {print \$3}")
+
+    # If oss.space dirs from the config don't already exist, create
+    # one. We use 'ls -l' here since "$space_dirs" can include
+    # wildcards (*).
+    # N.B. As long as a single path specified in an oss.space
+    # directive exists, XRootD is happy
+    if [[ -z $(ls -l "$space_dirs" 2> /dev/null) ]]; then
+        default_dir="$cache_dir/$dirtype"1
+        mkdir -p "$default_dir"
+        chown -R xrootd:xrootd "$default_dir"
+    fi
+done
