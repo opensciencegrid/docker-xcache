44f871659bca89b195ba3405615e83c74ee9eb97
Brian Lin <brianhlin@gmail.com>
2020-07-20 13:23:36 -0500

    Add more debugging to origin tests (SOFTWARE-4181)

--- tests/test_stashcache_origin.sh
+++ tests/test_stashcache_origin.sh
@@ -10,13 +10,15 @@ docker run --rm \
        --volume $(pwd)/tests/stashcache-origin-config/authfile:/etc/xrootd/public-origin-authfile \
        --volume $(pwd)/tests/stashcache-origin-config/test_file:/xcache/namespace/test_file \
        --name test_origin "$TEST_IMAGE" &
-docker ps 
 sleep 20
+docker ps
 
 online_md5="$(curl -sL http://localhost:1094/test_file | md5sum | cut -d ' ' -f 1)"
 local_md5="$(md5sum $(pwd)/tests/stashcache-origin-config/test_file | cut -d ' ' -f 1)"
+
 if [ "$online_md5" != "$local_md5" ]; then
     echo "MD5sums do not match on origin"
+    docker exec test_origin ls -lR /var/log/xrootd/
     docker exec test_origin cat /var/log/xrootd/stash-origin/xrootd.log
     docker stop test_origin
     exit 1
