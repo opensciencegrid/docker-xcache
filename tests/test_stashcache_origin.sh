bd0635acab81bf422c4d5f78943bb7c312c3222e
Brian Lin <brianhlin@gmail.com>
2020-07-20 12:55:46 -0500

    Fix container name for debugging origin test failures (SOFTWARE-4181)

--- tests/test_stashcache_origin.sh
+++ tests/test_stashcache_origin.sh
@@ -17,7 +17,7 @@ online_md5="$(curl -sL http://localhost:1094/test_file | md5sum | cut -d ' ' -f 
 local_md5="$(md5sum $(pwd)/tests/stashcache-origin-config/test_file | cut -d ' ' -f 1)"
 if [ "$online_md5" != "$local_md5" ]; then
     echo "MD5sums do not match on origin"
-    docker exec -it test_cache cat /var/log/xrootd/stash-origin/xrootd.log
+    docker exec -it test_origin cat /var/log/xrootd/stash-origin/xrootd.log
     docker stop test_origin
     exit 1
 fi
