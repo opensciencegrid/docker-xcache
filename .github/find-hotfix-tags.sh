8fa9598be8fc60094f6c0a02c44a3c1e516d6f4c
Brian Lin <blin@cs.wisc.edu>
2020-08-24 12:46:04 -0500

    Improve handling of JSON auth data (SOFTWARE-4151)
    Co-authored-by: edquist <edquist@users.noreply.github.com>

--- .github/find-hotfix-tags.sh
+++ .github/find-hotfix-tags.sh
@@ -3,10 +3,25 @@
 tag_regex='v[0-9]+\.[0-9]+\.[0-9]+-osghotfix-[A-Za-z]+'
 git tag -l | sort | egrep -x "$tag_regex" > git_tags
 
+mkjson () {
+  python -c '
+import sys
+import json
+
+def esplit(x):
+    return x.split("=", 1)
+
+data = dict(map(esplit, sys.argv[1:]))
+print(json.dumps(data, sort_keys=True))
+' "$@"
+}
+
+json_auth=$(mkjson username="$DOCKER_USERNAME" password="$DOCKER_PASSWORD")
+
 token=$(curl -s \
              -H "Content-Type: application/json" \
              -X POST \
-             -d '{"username": "'${DOCKER_USERNAME}'", "password": "'${DOCKER_PASSWORD}'"}' \
+             -d "$json_auth" \
              "https://hub.docker.com/v2/users/login/" | jq -r .token)
 
 curl -s \
