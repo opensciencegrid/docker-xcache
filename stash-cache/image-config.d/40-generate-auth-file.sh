27bd07e1061a4595bd4cbf592e554c62af4a61ac
Diego Davila <davila.foyo@gmail.com>
2021-10-20 12:52:01 -0700

    fixing aut update of the Authfile and scitokens.conf

--- stash-cache/image-config.d/40-generate-auth-file.sh
+++ stash-cache/image-config.d/40-generate-auth-file.sh
@@ -4,3 +4,16 @@
 /usr/libexec/xcache/authfile-update --cache
 chown xrootd:xrootd /run/stash-cache-auth/*
 chown xrootd:xrootd /run/stash-cache/*
+
+# ddavila 20211020: Save the env vars CACHE_FQDN or ORIGIN_FQDN to be used
+# later by 'xrootd' on the 'authfile-update' script.
+echo "# This file was genrated on startup" > /etc/xrootd-environment
+
+if [ ! -z ${CACHE_FQDN} ]; then
+        echo "export CACHE_FQDN=${CACHE_FQDN}" >> /etc/xrootd-environment
+fi
+
+if [ ! -z ${ORIGIN_FQDN} ]; then
+        echo "export ORIGIN_FQDN=${ORIGIN_FQDN}" >> /etc/xrootd-environment
+fi
+chown xrootd:xrootd /etc/xrootd-environment
