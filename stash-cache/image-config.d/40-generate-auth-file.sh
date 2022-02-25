2a6043b2526217315fb08f1617d0fc63a40a9639
Brian Lin <brianhlin@gmail.com>
2022-02-25 08:58:29 -0600

    Fix typos

--- stash-cache/image-config.d/40-generate-auth-file.sh
+++ stash-cache/image-config.d/40-generate-auth-file.sh
@@ -10,7 +10,7 @@ shopt -u nullglob
 
 # ddavila 20211020: Save the env vars CACHE_FQDN to be used
 # later by 'xrootd' on the 'authfile-update' script.
-echo "# This file was genrated on startup" > /etc/xrootd-environment
+echo "# This file was generated on startup" > /etc/xrootd-environment
 
 if [[ -n ${CACHE_FQDN} ]]; then
         echo "export CACHE_FQDN=${CACHE_FQDN}" >> /etc/xrootd-environment
