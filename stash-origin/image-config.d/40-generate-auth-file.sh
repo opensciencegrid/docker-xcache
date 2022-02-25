fe7bcefce5e82e0280509aff84bc630a1bf2df86
Diego Davila <davila.foyo@gmail.com>
2022-02-24 18:41:18 -0800

    make sure the FQDN_ORING env var is saved for later use in the cronjob

--- stash-origin/image-config.d/40-generate-auth-file.sh
+++ stash-origin/image-config.d/40-generate-auth-file.sh
@@ -7,3 +7,12 @@ for f in /run/stash-origin/* /run/stash-origin-auth/*; do
     chown xrootd:xrootd "$f"
 done
 shopt -u nullglob
+
+# ddavila 20221020: Save the env var ORIGIN_FQDN to be used
+# later by 'xrootd' on the 'authfile-update' script.
+echo "# This file was genrated on startup" > /etc/xrootd-environment
+
+if [[ -n ${ORIGIN_FQDN} ]]; then
+        echo "export ORIGIN_FQDN=${ORIGIN_FQDN}" >> /etc/xrootd-environment
+fi
+chown xrootd:xrootd /etc/xrootd-environment
